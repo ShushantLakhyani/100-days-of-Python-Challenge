a73eb1bfb82ea14d104841754e03f63e0a518850
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-08-15 08:27:45 +0530

    Create coin_toss_program.py

---
+++ coin_toss_program.py
@@ -0,0 +1,7 @@
+#This program is for the coin toss algorithm. This requires the application of the random function.
+import random
+random_number = random.randint(0,1)
+if random_number == 1:
+  print ('Heads')
+else:
+  print ('Tails')
