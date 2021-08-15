a8d14189643bf33255ae33679ba3e6b42edff919
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-08-15 12:06:56 +0530

    Create treasuremap.py

---
+++ treasuremap.py
@@ -0,0 +1,12 @@
+#The code is a game in which the user haas to input the row and column number and the output will show an 'X' mark at the matrix
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+horizontal = int(position[0])
+vertical = int(position[1])
+
+map[vertical-1][horizontal-1] = 'X'
+print(f"{row1}\n{row2}\n{row3}")
