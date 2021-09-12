a547cef7ed79189d9fc5d95af7b851b4d1322fab
Shushant Lakhyani <39303292+ShushantLakhyani@users.noreply.github.com>
2021-09-12 18:13:28 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,4 @@
-#This programs output will create a passwor for you, as per your needs
+#This programs output will create a password for you, as per your needs
 #you just have to enter the number of letters, numbers and symbols you need in the password
 import random
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
